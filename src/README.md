a7c571ba76aba7bbf2bbda028b8a862864380ec3
fcojmoron <f.moronreyes@dxc.com>
2025-09-12 08:33:29 +0200

    Create first version of generative answer

---
+++ src/README.md
@@ -0,0 +1,4 @@
+# https://huggingface.co/spaces/mteb/leaderboard
+
+GENERATION: https://huggingface.co/Qwen/Qwen3-0.6B
+EMBEDDING: https://huggingface.co/Qwen/Qwen3-Embedding-0.6B
--- src/app/config.py
+++ src/app/config.py
@@ -13,6 +13,7 @@ class DataStorageConfig(BaseModel):
 class ModelConfig(BaseModel):
     embedding_model: str = os.getenv("EMBEDDING_MODEL", "Qwen/Qwen3-Embedding-0.6B")
     embedding_dim: int = os.getenv("EMBEDDING_DIM", 512)
+    generative_model: str = os.getenv("GENERATIVE_MODEL", "Qwen/Qwen3-0.6B")
 
 class ChunkConfig(BaseModel):
     chunk_size: int = os.getenv("CHUNK_SIZE", 500)
